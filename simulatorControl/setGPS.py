b65d5d39a54664582f015badeede35191f9eb0ca
sougannkyou <songyq0509@163.com>
2018-05-11 13:07:52 +0800

    @company

---
+++ simulatorControl/setGPS.py
@@ -0,0 +1,10 @@
+import os
+
+
+def setNoxGPS(latitude, longitude):
+    print(latitude, longitude)  # 39.6099202570, 118.1799316404
+    p = os.popen("adb shell setprop persist.nox.gps.latitude " + latitude)
+    print(p.read())
+
+    p = os.popen("adb shell setprop persist.nox.gps.longitude " + longitude)
+    print(p.read())
