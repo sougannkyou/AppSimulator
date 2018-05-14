75bcf3976cbb9298cbd110b4da24d845ef277412
sougannkyou <songyq0509@163.com>
2018-05-14 23:32:57 +0800

    @homr

--- simulatorControl/script1.py
+++ simulatorControl/script1.py
@@ -12,39 +12,23 @@ import pyautogui
 
 DEBUG_ENV = False
 
-PIC_PATH = {
-    u"APP图标": 'images/app_ready.png',
-    u"更新": 'images/update.png',
-    u"分享": 'images/share.png',
-    u"复制链接": 'images/copylink.png',
-    u"跳过软件升级": 'images/is_upgrade.png',
-    u"锁屏": 'images/screen_lock.png'
-}
-
-CLICK_POS = {
-    u"APP图标": (38, 793),
-    u"更新": (38, 793),
-    u"分享": (451, 628),
-    u"复制链接": (47, 720),
-    u"跳过软件升级": (231, 590)  # 以后再说
-}
-
-UNLOCK_POS = {
-    "step1": (133, 496),
-    "step2": (132, 705),
-    "step3": (343, 707)
-}
-
-
 class Simulator:
     def __init__(self, app_name):
+        self._UNLOCK_POS = {
+            "step1": (133, 496),
+            "step2": (132, 705),
+            "step3": (343, 707)
+        }
+        self._PIC_PATH = {}
+        self._CLICK_POS = {}
+        self.is_proxy_active = True
         self.hwnd = win32gui.FindWindow(None, app_name)
 
     def is_upgrade(self, img_capture, comment):
-        img_obj = ac.imread(PIC_PATH[comment])
+        img_obj = ac.imread(self._PIC_PATH[comment])
         pos = ac.find_template(img_capture, img_obj)
         if pos and pos['confidence'] > 0.9:
-            print('版本更新提示', PIC_PATH[comment])
+            print('版本更新提示', self._PIC_PATH[comment])
             self.click(u"跳过软件升级", 1)
             self.click(u"分享", 1)
 
@@ -53,16 +37,23 @@ class Simulator:
         win32gui.SetForegroundWindow(self.hwnd)
         # print(left, top, right, bottom)
         # print(right - left, bottom - top)
-        (x, y) = CLICK_POS[comment]
+        (x, y) = self._CLICK_POS[comment]
         x = left + x
         y = top + y
-        time.sleep(timeout)
+        self._sleep(timeout)
         win32api.SetCursorPos((x, y))
         win32api.mouse_event(WCON.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
         win32api.mouse_event(WCON.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
-        time.sleep(timeout)
+        self._sleep(timeout)
         print("click", comment)
         return True
+
+    def _sleep(self, times):
+        while (not self.is_proxy_active):
+            print("wait for proxy server active ...")
+            time.sleep(1)
+        else:
+            time.sleep(times)
 
     def _detect_obj(self, img, circle_center_pos, circle_radius, color, line_width):
         if DEBUG_ENV:
@@ -80,7 +71,7 @@ class Simulator:
     def find_element(self, comment, timeout):
         # 匹配element图像
         # pprint(pos)
-        obj_pic_path = PIC_PATH[comment]
+        obj_pic_path = self._PIC_PATH[comment]
         while (timeout > 0):
             win32gui.SetForegroundWindow(self.hwnd)
             left, top, right, bottom = win32gui.GetWindowRect(self.hwnd)
@@ -101,7 +92,7 @@ class Simulator:
                 return True
             else:
                 print('未匹配到', comment, timeout)
-                time.sleep(1)
+                self._sleep(1)
                 timeout -= 1
                 self.is_upgrade(img_capture, comment=u"跳过软件升级")
 
@@ -111,17 +102,17 @@ class Simulator:
         win32gui.SetForegroundWindow(self.hwnd)
         # win32api.SetCursorPos([left + 10, top + 10])
         # win32api.mouse_event(WCON.MOUSEEVENTF_LEFTDOWN,0,0,0,0)
-        # time.sleep(0.3)
+        # self._sleep(0.3)
         # win32api.mouse_event(WCON.MOUSEEVENTF_LEFTUP,0,0,0,0)
         # win32api.SendMessage(hwnd,WCON.WM_SETTEXT,None,'A')
 
         # 向窗口发送Enter键
-        time.sleep(timeout)
+        self._sleep(timeout)
         win32api.keybd_event(key_value, 0, 0, 0)
-        # time.sleep(0.5)
+        # self._sleep(0.5)
         win32api.keybd_event(key_value, 0, WCON.KEYEVENTF_KEYUP, 0)
         print('发送', key_value, '键')
-        time.sleep(timeout)
+        self._sleep(timeout)
         return True
 
     def app_quit(self):
@@ -135,27 +126,27 @@ class Simulator:
         win32gui.SetForegroundWindow(self.hwnd)
         left, top, right, bottom = win32gui.GetWindowRect(self.hwnd)
         top += 20
-        time.sleep(timeout)
+        self._sleep(timeout)
 
-        (x, y) = UNLOCK_POS['step1']
+        (x, y) = self._UNLOCK_POS['step1']
         x = left + x
         y = top + y
         pyautogui.moveTo(x, y)
         pyautogui.mouseDown()
 
-        (x, y) = UNLOCK_POS['step2']
+        (x, y) = self._UNLOCK_POS['step2']
         x = left + x
         y = top + y
         # pyautogui.dragTo(x, y, 0.5, button='left')
         pyautogui.moveTo(x, y, 1, pyautogui.easeInQuad)
 
-        (x, y) = UNLOCK_POS['step3']
+        (x, y) = self._UNLOCK_POS['step3']
         x = left + x
         y = top + y
         # pyautogui.dragTo(x, y, 0.5, button='left')
         pyautogui.moveTo(x, y, 1, pyautogui.easeInBounce)
         pyautogui.mouseUp()
-        time.sleep(timeout)
+        self._sleep(timeout)
         return True
 
     def run(self):
@@ -168,6 +159,7 @@ class Simulator:
 
     def script(self):
         pass
+
 
 if __name__ == "__main__":
     class MySimulator(Simulator):
@@ -186,5 +178,22 @@ if __name__ == "__main__":
                 if ret: ret = self.click(u"复制链接", timeout=1)
                 if not ret: self.send2web('images/offline.png')
 
+
     me = MySimulator("douyin0")
+    me._PIC_PATH = {
+        u"APP图标": 'images/app_ready.png',
+        u"更新": 'images/update.png',
+        u"分享": 'images/share.png',
+        u"复制链接": 'images/copylink.png',
+        u"跳过软件升级": 'images/is_upgrade.png',
+        u"锁屏": 'images/screen_lock.png'
+    }
+
+    me._CLICK_POS = {
+        u"APP图标": (38, 793),
+        u"更新": (38, 793),
+        u"分享": (451, 628),
+        u"复制链接": (47, 720),
+        u"跳过软件升级": (231, 590)  # 以后再说
+    }
     me.run()
