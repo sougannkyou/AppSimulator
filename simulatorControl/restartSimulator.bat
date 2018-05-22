0791c5eb604c17b2ee1f8a8763a9cf6f7c805961
sougannkyou <songyq0509@163.com>
2018-05-22 14:44:22 +0800

    @company

--- simulatorControl/restartSimulator.bat
+++ simulatorControl/restartSimulator.bat
@@ -2,7 +2,8 @@ C:\Nox\bin\Nox.exe -quit
 timeout 10
 start /b C:\Nox\bin\Nox.exe
 timeout 30
-taskkill /f /t /fi "WINDOWTITLE eq script"
-timeout 10
 cd %RPCSERVER_HOME%
 python script_douyin.py
+timeout 10
+C:\Nox\bin\Nox.exe -quit
+
