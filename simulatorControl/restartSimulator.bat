cc834e59b9de7be06d60f670f8a5e971f6acdb57
sougannkyou <syq314159>
2018-05-29 09:06:35 +0800

    @device

--- simulatorControl/restartSimulator.bat
+++ simulatorControl/restartSimulator.bat
@@ -1,12 +1,12 @@
 rem taskkill /t /f /fi "WINDOWTITLE eq taskeng.exe"
-C:\Nox\bin\Nox.exe -quit
+start /b start C:\Nox\bin\Nox.exe -quit
 timeout 10
-start /b /HIGH C:\Nox\bin\Nox.exe
+start /b start /HIGH C:\Nox\bin\Nox.exe
 timeout 60
 tasklist | findstr "Nox.exe"
 cd %SIMULATOR_CONTROL_HOME%
 start /b start /HIGH python script_douyin.py
-timeout 10
 rem C:\Nox\bin\Nox.exe -quit
-rem timeout 30
+timeout 10
+exit
 
