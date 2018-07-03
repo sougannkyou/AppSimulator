8c5915b21ee35ab300a46510c72cee637c382bcf
sougannkyou <songyq0509@163.com>
2018-07-03 16:15:57 +0800

    @company

--- ControllerGUI/StartScript.bat
+++ ControllerGUI/StartScript.bat
@@ -5,7 +5,7 @@ start /HIGH C:\Nox\bin\Nox.exe
 timeout 90
 REM tasklist | findstr "Nox.exe"
 cd %APPSIMULATOR_WORK_PATH%
-start "script" /HIGH python Controller/script_douyin.py
+start "script" /HIGH python ControllerGUI/script_douyin.py
 timeout 10
 exit
 
