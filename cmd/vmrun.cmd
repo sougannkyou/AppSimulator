9a494015e2de681df3b8ae121e184c9e3b409667
sougannkyou <syq314159>
2018-07-04 10:13:03 +0800

    @company

--- cmd/vmrun.cmd
+++ cmd/vmrun.cmd
@@ -1,19 +1,19 @@
 cd "C:\VMware\Workstation"
 
 REM echo stop vm
-REM vmrun.exe stop "C:\VM\device1\Windows 7 x64.vmx"
+REM vmrun.exe stop "C:\VMWare\VM\device1\Windows 7 x64.vmx"
 REM timeout 10
 
 rem echo snapshot vm 
-rem vmrun.exe snapshot "c:\VM\device1\Windows 7 x64.vmx" device1-snapshot
+rem vmrun.exe snapshot "c:\VMWare\VM\device1\Windows 7 x64.vmx" device1-snapshot
 rem timeout 10
 
 echo revertToSnapshot 
-vmrun.exe revertToSnapshot "c:\VM\device1\Windows 7 x64.vmx" device1-snapshot
+vmrun.exe revertToSnapshot "c:\VMWare\VM\device1\Windows 7 x64.vmx" snapshot-device1
 timeout 10
 
 echo start vm
-vmrun.exe start "c:\VM\device1\Windows 7 x64.vmx"
+vmrun.exe start "c:\VMWare\VM\device1\Windows 7 x64.vmx"
 timeout 10
 
-vmrun.exe  -T ws -gu "zhxg" -gp "zhxg2018" runPrograminGuest "c:\VMWare\device1\Windows 7 x64.vmx" "C:\workspace\AppSimulator\Controller\script_douyin.cmd"
+vmrun.exe  -T ws -gu "zhxg" -gp "zhxg2018" runPrograminGuest "c:\VMWare\VM\device1\Windows 7 x64.vmx" "C:\workspace\pyWorks\AppSimulator\Controller\script_douyin.cmd"
