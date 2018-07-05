b0876e8bed45961e65103830077ca1bb7eaa50ea
sougannkyou <songyq0509@163.com>
2018-07-05 08:55:36 +0800

    @company

--- cmd/script_douyin.cmd
+++ cmd/script_douyin.cmd
@@ -1,4 +1,8 @@
-
+C:\Nox\bin\Nox.exe -quit
+timeout 10
+start /HIGH C:\Nox\bin\Nox.exe
+timeout 90
+REM tasklist | findstr "Nox.exe"
 cd %APPSIMULATOR_WORK_PATH%
 start "script" /HIGH python Controller/script_douyin.py
 timeout 10
