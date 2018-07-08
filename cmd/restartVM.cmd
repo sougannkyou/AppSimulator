6bf754f3b864b04fb14ac9e0da651b52c51be4c5
sougannkyou <songyq0509@163.com>
2018-07-09 00:53:39 +0800

    @home

--- cmd/restartVM.cmd
+++ cmd/restartVM.cmd
@@ -1,15 +1,25 @@
-@ECHO OFF
+REM @ECHO OFF
 SET vmrun=C:\VMware\Workstation\vmrun.exe
 SET workspace=d:\VM
 SET vmx=Windows 7 x64.vmx
 SET vm=%1
 
+IF NOT DEFINED vm (
+    ECHO [error] please input vm name
+    GOTO END
+)
+
 ECHO [%DATE% %TIME%] stop %vm%
 %vmrun% stop "%workspace%\%vm%\%vmx%"
+
 ECHO [%DATE% %TIME%] delete %vm%
 DEL /F /Q /S %workspace%\%vm%
+
 ECHO [%DATE% %TIME%] copy %vm%
 XCOPY /Y %workspace%\%vm%-org %workspace%\%vm%
+
 ECHO [%DATE% %TIME%] start %vm%
 %vmrun% start "%workspace%\%vm%\%vmx%"
+
+:END
 @ECHO ON
