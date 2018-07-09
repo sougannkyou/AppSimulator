eb9fd213c7f67acc2f1317575b14c763ada853c2
sougannkyou <songyq0509@163.com>
2018-07-09 13:24:51 +0800

    @company

--- cmd/StartScript.cmd
+++ cmd/StartScript.cmd
@@ -1,3 +1,4 @@
+@ECHO OFF
 timeout 10
 cd %APPSIMULATOR_WORK_PATH%
 set /P taskConf= 0<cmd\task.conf
@@ -5,6 +6,7 @@ echo %taskConf%
 timeout 60
 start "script" /HIGH python Controller/script_%taskConf%.py
 timeout 10
+@ECHO ON
 exit
 
 
