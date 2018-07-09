b64c7295b129175eaa90b9423422d7ce107b8ec5
sougannkyou <songyq0509@163.com>
2018-07-09 12:58:39 +0800

    @company

--- Controller/script_miaopai.py
+++ Controller/script_miaopai.py
@@ -1,6 +1,6 @@
 # coding:utf-8
 import os
-import sys
+import systype
 
 sys.path.append(os.getcwd())
 
--- cmd/StartScript.cmd
+++ cmd/StartScript.cmd
@@ -1,12 +1,8 @@
-REM taskkill /t /f /fi "WINDOWTITLE eq script"
-REM C:\Nox\bin\Nox.exe -quit
-REM timeout 10
-REM start /HIGH C:\Nox\bin\Nox.exe
+timeout 10
+cd %APPSIMULATOR_WORK_PATH%
 set /P taskConf= 0<cmd\task.conf
 echo %taskConf%
 timeout 60
-REM tasklist | findstr "Nox.exe"
-cd %APPSIMULATOR_WORK_PATH%
 start "script" /HIGH python Controller/script_%taskConf%.py
 timeout 10
 exit
