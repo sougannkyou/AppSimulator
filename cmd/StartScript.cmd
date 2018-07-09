ccb20ae2b63d7ff26b58f13ccbaaa8c00db828cd
sougannkyou <songyq0509@163.com>
2018-07-09 20:27:29 +0800

    @home

--- cmd/StartScript.cmd
+++ cmd/StartScript.cmd
@@ -1,14 +1,12 @@
-@ECHO OFF
+@ECHO ON
 timeout 10
-echo 退出模拟器
 C:\Nox\bin\Nox.exe -quit
 timeout 10
-echo 启动模拟器
 C:\Nox\bin\Nox.exe
 timeout 10
 cd %APPSIMULATOR_WORK_PATH%
 set /P taskConf= 0<cmd\task.conf
-echo 开始执行脚本 %taskConf%
+echo "start script " %taskConf%
 timeout 60
 start "script" /HIGH python Controller/script_%taskConf%.py
 timeout 10
