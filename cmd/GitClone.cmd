c61942b9845dbcc5c9378771e359f3ad940e3b5a
sougannkyou <songyq0509@163.com>
2018-07-12 14:35:37 +0800

    @company

--- cmd/GitClone.cmd
+++ cmd/GitClone.cmd
@@ -1,7 +1,7 @@
 @ECHO OFF
 echo now update source from git
 cd %APPSIMULATOR_WORK_PATH%\..
-DEL /F /Q /S AppSimulator
+REDIR /Q /S AppSimulator
 REM C:\Git\bin\git pull https://github.com/sougannkyou/AppSimulator.git
 C:\Git\bin\git clone https://github.com/sougannkyou/AppSimulator.git
 timeout 30
