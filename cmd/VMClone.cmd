c45fc79a9db9412df382ad298b8c54f7332d1225
sougannkyou <songyq0509@163.com>
2018-07-18 15:05:43 +0800

    @company

--- cmd/VMClone.cmd
+++ cmd/VMClone.cmd
@@ -1,62 +1,59 @@
 @ECHO OFF
-SET vmrun=C:\VMware\Workstation\vmrun.exe
-SET workspace=d:\VMware\VM
-SET vmx=Windows 7 x64.vmx
+::--------------------------------------------------
+SET VMRUN_EXE=C:\VMware\Workstation\vmrun.exe
+SET workspace=D:\VMware\VM
+SET VMX_FILE=Windows 7 x64.vmx
+::---- main ----------------------------------------
+FOR %%vm in (vm1,vm2,vm3,vm4,vm5) do CALL :STOP_VM %%vm
 
-FOR %%i in (vm1,vm2,vm3,vm4,vm5) do  CALL STOP_VM %%i
+CALL :KILL_VM
 
+FOR %%vm in (vm1,vm2,vm3,vm4,vm5) do CALL :CLONE_VM %%vm
+
+FOR %%vm in (vm1,vm2,vm3,vm4,vm5) do CALL :START_VM %%vm
+
+GOTO :EOF
+::--------------------------------------------------
+:KILL_VM
 TASKKILL /F /IM "VMware.exe"
-ECHO TASKKILL ret %ERRORLEVEL%
-
-FOR %%i in (vm1,vm2,vm3,vm4,vm5) do  CALL CLONE_VM %%i
-
-FOR %%i in (vm1,vm2,vm3,vm4,vm5) do  CALL START_VM %%i
-
+ECHO [%DATE% %TIME%] TASKKILL %ERRORLEVEL%
+GOTO :EOF
 ::--------------------------------------------------
 :STOP_VM
 
-SET vm=vm%1
-
+SET vm=%1
 IF NOT DEFINED vm (
-    ECHO [error] please input vm name
+    ECHO [ERROR] please input vm name
     GOTO END
 )
 
-ECHO [%DATE% %TIME%] stop %vm%
-%vmrun% stop "%workspace%\%vm%\%vmx%"
-ECHO STOP_VM %ERRORLEVEL%
+%VMRUN_EXE% stop "%workspace%\%vm%\%VMX_FILE%"
+ECHO [%DATE% %TIME%] STOP_VM %vm% : %ERRORLEVEL%
 GOTO :EOF
-
 ::--------------------------------------------------
 :START_VM
-SET vm=vm%1
 
+SET vm=%1
 IF NOT DEFINED vm (
-    ECHO [error] please input vm name
+    ECHO [ERROR] please input vm name
     GOTO END
 )
-ECHO [%DATE% %TIME%] start %vm%
-%vmrun% start "%workspace%\%vm%\%vmx%"
-ECHO START_VM %ERRORLEVEL%
+%VMRUN_EXE% start "%workspace%\%vm%\%VMX_FILE%"
+ECHO [%DATE% %TIME%] START_VM %vm% : %ERRORLEVEL%
 GOTO :EOF
-
 ::--------------------------------------------------
 :CLONE_VM
-
-SET vm=vm%1
+SET vm=%1
 
 IF NOT DEFINED vm (
-    ECHO [error] please input vm name
+    ECHO [ERROR] please input vm name
     GOTO END
 )
-
-ECHO [%DATE% %TIME%] delete %vm%
+ECHO [%DATE% %TIME%] DELETE %vm%
 DEL /F /Q /S %workspace%\%vm%
-
-ECHO [%DATE% %TIME%] copy %vm%
+ECHO [%DATE% %TIME%] XCOPY %vm%
 XCOPY /Y %workspace%\%vm%-org %workspace%\%vm%
-
-ECHO CLONE_VM %ERRORLEVEL%
+ECHO [%DATE% %TIME%] CLONE_VM %vm% : %ERRORLEVEL%
 GOTO :EOF
 ::--------------------------------------------------
 
