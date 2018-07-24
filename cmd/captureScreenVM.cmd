36f8dc1dbd47f631dac8df9c01479ae23f1cf25c
sougannkyou <songyq0509@163.com>
2018-07-24 09:02:41 +0800

    @company

--- cmd/captureScreenVM.cmd
+++ cmd/captureScreenVM.cmd
@@ -4,13 +4,15 @@ SET workspace=d:\VMware\VM
 SET vmx=Windows 7 x64.vmx
 
 FOR %%vm in (vm1,vm2,vm3,vm4,vm5) DO CALL :CAPTURESCREEN_VM %vm
-
 GOTO :EOF
 ::--------------------------------------------------
 :CAPTURESCREEN_VM
 SET vmName=%1
-ECHO vmrun.exe  -T ws -gu win7_64 -gp zhxg2018 captureScreen "%workspace%\%vmName%\%vmx%" "%APPSIMULATOR_WORK_PATH%\\static\\images\\AppSimulator\\images\\VM\"
+SET before="%APPSIMULATOR_WORK_PATH%\static\images\AppSimulator\images\VM\capture_%vmName%.png"
+SET current="%APPSIMULATOR_WORK_PATH%\static\images\AppSimulator\images\VM\capture_%vmName%_before.png"
+MOVE %current% %before%
+ECHO vmrun.exe  -T ws -gu win7_64 -gp zhxg2018 captureScreen "%workspace%\%vmName%\%vmx%" %current%
 GOTO :EOF
-
+::--------------------------------------------------
 :END
 ECHO ON
