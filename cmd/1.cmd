7a92d2282a6cf75c09d9e3bf16c64f455b7db196
sougannkyou <songyq0509@163.com>
2018-07-25 17:22:16 +0800

    @company

--- cmd/1.cmd
+++ cmd/1.cmd
@@ -1,1 +1,12 @@
-for /L %%i in (1 1 12) do echo %%i
+
+FOR %%i in (1,2,3,4,5) DO CALL :LOOP_VM
+GOTO :EOF
+
+:LOOP_VM
+FOR %%vm in (vm1,vm2,vm5) (
+   echo %vm%
+)
+GOTO :EOF
+::--------------------------------------------------
+:END
+ECHO ON
