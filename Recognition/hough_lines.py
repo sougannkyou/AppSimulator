760b06254c2b2e4b6d3bdf08ea972ee772bf7872
sougannkyou <songyq0509@163.com>
2018-07-31 17:30:36 +0800

    @company

---
+++ Recognition/hough_lines.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('videos.png')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+edges = cv2.Canny(gray, 100, 120)
+minLineLength = 10000
+maxLineGap = 100
+lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 20, minLineLength, maxLineGap)
+for x1, y1, x2, y2 in lines[0]:
+    cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+
+cv2.imshow("edges", edges)
+cv2.imshow("lines", img)
+cv2.waitKey()
+cv2.destroyAllWindows()
