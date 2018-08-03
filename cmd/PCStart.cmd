fc994c93cf7ca58159fc1b6f811a216bfee04bad
sougannkyou <songyq0509@163.com>
2018-08-03 10:15:19 +0800

    @company

--- cmd/PCStart.cmd
+++ cmd/PCStart.cmd
@@ -1,6 +1,7 @@
 TIMEOUT 60
 cd %APPSIMULATOR_WORK_PATH%\cmd
-VMClone.cmd
-TIMEOUT 60
-START "WebServer" WebStart.cmd
+START "Revert All VMware" VMClone.cmd
+TIMEOUT 1200
 START "VMware Screen Capture" VMScreenCapture.cmd
+TIMEOUT 10
+START "WebServer" WebStart.cmd
