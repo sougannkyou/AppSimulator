2572e6f8eb9c8178ca6b8c1128253a68733cc163
sougannkyou <songyq0509@163.com>
2018-08-07 23:18:52 +0800

    ``

--- Controller/script_dianping_comments.py
+++ Controller/script_dianping_comments.py
@@ -24,91 +24,111 @@ class MySelenium(NoxConSelenium):
     def __init__(self, task_info, mode):
         super().__init__(task_info=task_info, mode=mode)
 
-    def pic_to_ocr(self):
-        # start = datetime.now()
-        capture_name = 'capture_' + self._docker_name + '.png'
-        capture_before_name = 'capture_' + self._docker_name + '_before.png'
-        capture_path = self._work_path + '\\Controller\\images\\temp\\' + capture_name
-        capture_before_path = self._work_path + '\\Controller\\images\\temp\\' + capture_before_name
+        self.capture_name = 'capture_' + self._docker_name + '.png'
+        self.capture_before_name = 'capture_' + self._docker_name + '_before.png'
+        self.capture_path = self._work_path + '\\Controller\\images\\temp\\' + self.capture_name
+        self.capture_before_path = self._work_path + '\\Controller\\images\\temp\\' + self.capture_before_name
 
-        capture_name_new = 'capture_' + self._docker_name + '_new.png'
-        capture_before_name_new = 'capture_' + self._docker_name + '_new_before.png'
-        capture_path_new = self._work_path + '\\Controller\\images\\temp\\' + capture_name_new
-        capture_before_path_new = self._work_path + '\\Controller\\images\\temp\\' + capture_before_name_new
+        self.capture_cut_name = 'capture_' + self._docker_name + '_cut.png'
+        self.capture_before_cut_name = 'capture_' + self._docker_name + '_before_cut.png'
+        self.capture_cut_path = self._work_path + '\\Controller\\images\\temp\\' + self.capture_cut_name
+        self.capture_before_cut_path = self._work_path + '\\Controller\\images\\temp\\' + self.capture_before_cut_name
 
-        capture_concat_name = 'capture_' + self._docker_name + '_concat.png'
-        capture_concat_name_new = 'capture_' + self._docker_name + '_concat_new.png'
-        capture_concat_path = self._work_path + '\\Controller\\images\\temp\\' + capture_concat_name
-        capture_concat_path_new = self._work_path + '\\Controller\\images\\temp\\' + capture_concat_name_new
+        self.capture_comment_name = 'capture_' + self._docker_name + '_comment.png'
+        self.capture_comment_cut_name = 'capture_' + self._docker_name + '_comment_cut.png'
+        self.capture_comment_path = self._work_path + '\\Controller\\images\\temp\\' + self.capture_comment_name
+        self.capture_comment_cut_path = self._work_path + '\\Controller\\images\\temp\\' + self.capture_comment_cut_name
 
         # border_path = self._work_path + '\\Controller\\images\\dianping\\border.png'
-        border_path = self._work_path + '\\Controller\\images\\dianping\\border_128_128.png'
+        self.border_path = self._work_path + '\\Controller\\images\\dianping\\border_128_128.png'
 
-        # 前后页分别截图
-        img = Image.open(capture_before_path)
-        # img = img.crop((0, 0, 480, 750))
-        img.save(capture_before_path_new)
-
-        img = Image.open(capture_path)
-        # img = img.crop((0, 75, 480, 750))
-        img.save(capture_path_new)
-
-        # 800 * 480
-        img1 = cv2.imread(capture_before_path_new)
-        img2 = cv2.imread(capture_path_new)
-
-        # 纵向合成
-        img_bg = np.vstack((img1, img2))
-        cv2.imwrite(capture_concat_path, img_bg)
-
-        # 照片图框 152 * 152
-        img_border = ac.imread(border_path)
-
+    def get_photo_top_y(self, img_obj):
+        # 照片图框 152 * 152 or 128 * 128
+        img_border = ac.imread(self.border_path)
         h, w, _ = img_border.shape
-        ret = ac.find_all_template(img_bg, img_border, threshold=0.5)
+        ret = ac.find_all_template(img_obj, img_border, threshold=0.5)
         l = []
         for r in ret:
             (x, y) = r['result']
             l.append(y)
 
         if l:
-            min_y = min(l)
-            print(min_y)
+            ret = min(l)
+        else:
+            ret = -1
+
+        return ret
+
+    def pic_to_ocr(self, one_page, photo_top_y, page_line_y):
+        # start = datetime.now()
+        if one_page:
+            y = 800
+            img = Image.open(self.capture_path)
+            if photo_top_y != -1:
+                y = photo_top_y
+            else:
+                if page_line_y != -1:
+                    y = page_line_y
+
+            img = img.crop((0, 75, 480, y))
+            img.save(self.capture_comment_cut_path)
+
+        else:  # page_line_y is next page y
+            # 前后页分别截图
+            img = Image.open(self.capture_before_path)
+            # img = img.crop((0, 0, 480, 750))
+            img.save(self.capture_before_cut_path)
+
+            img = Image.open(self.capture_path)
+            img = img.crop((0, 75, 480, 800))
+            img.save(self.capture_cut_path)
+
+            # 800 * 480
+            img1 = cv2.imread(self.capture_before_cut_path)
+            img2 = cv2.imread(self.capture_cut_path)
+
+            # 纵向合成
+            img_concat = np.vstack((img1, img2))
+            cv2.imwrite(self.capture_comment_path, img_concat)
 
             # 合成图截图
-            img = Image.open(capture_concat_path)
+            img = Image.open(self.capture_comment_path)
             img = img.crop((0, 180, 480, min_y - h / 2))
-            img.save(capture_concat_path_new)
+            img.save(self.capture_comment_cut_path)
 
-            image = Image.open(capture_concat_path_new)
+            image = Image.open(self.capture_comment_cut_path)
             code = pytesseract.image_to_string(image, lang='chi_sim')
-            print('-------------------\n', code)
-        else:
-            print('-------------------\n', 'not found photo')
+            if code:
+                print('-------------------\n', code)
+            else:
+                print('-------------------\n', 'not found comment')
 
     def script(self):
-        ret = True
-        x = -1
-        y = -1
-        page_cnt = 0
         # ret, x, y = self.find_element(comment='APP打开结果OK', timeout=60)
-        # _pos_list = []
-        # fail = 0
         # while ret:
         self.next_page(from_y=210, to_y=10, wait_time=1)
 
-        ret, x, y = self.find_element(comment='全文', timeout=10)
+        ret, x, y = self.find_element(comment='展开全文', timeout=10)
         if ret:
-            ret = self.click_xy(x, y, wait_time=1)
+            self.click_xy(x, y, wait_time=1)
             self.get_capture()
 
         # nox_adb.exe shell input swipe 240 670 240 10 2000
         # nox_adb.exe shell screencap -p /sdcard/capture.png
         # nox_adb.exe pull /sdcard/capture.png c:\Nox\
-        ret = self.next_page(from_y=670, to_y=10, wait_time=1)
-        ret, x, y = self.find_element(comment='分页', timeout=10)
+        ret, _, page_line_y = self.find_element(comment='分页线', timeout=10)
         if ret:
-            self.pic_to_ocr()
+            img_obj = Image.open(self.capture_path)
+            photo_top_y = self.get_photo_top_y(img_obj)
+        else:
+            ret = self.next_page(from_y=670, to_y=10, wait_time=1)
+            self.get_capture()  # 更新截图
+            img_obj = Image.open(self.capture_path)
+            photo_top_y = self.get_photo_top_y(img_obj)
+            if photo_top_y == -1:
+                ret, _, page_line_y = self.find_element(comment='分页线', timeout=10)
+
+        self.pic_to_ocr(one_page=ret, photo_top_y=photo_top_y, page_line_y=page_line_y)
 
         # self.back()
         # ret = self.next_page(wait_time=1)
@@ -134,8 +154,8 @@ def main(task, mode):
             "分享": 'images/dianping/share.png',
             "复制链接": 'images/dianping/copy_link.png',
             "打分": 'images/dianping/dafen.png',
-            "全文": 'images/dianping/show_all.png',
-            "分页": 'images/dianping/page_line.png',
+            "展开全文": 'images/dianping/show_all.png',
+            "分页线": 'images/dianping/page_line.png',
             "star": 'images/dianping/star.png',
         })
         me._DEBUG = True
@@ -152,7 +172,7 @@ def main(task, mode):
 #################################################################################
 if __name__ == "__main__":
     _DEBUG = True
-
+    print("start")
     if APPSIMULATOR_MODE == 'vmware':
         taskId = -1
         timer_no = -1
