c1b637a3b78077938bc033e821a487b67a3d4810
sougannkyou <songyq0509@163.com>
2018-08-08 08:56:29 +0800

    @company

--- Controller/script_dianping_comments_onepage.py
+++ Controller/script_dianping_comments_onepage.py
@@ -24,7 +24,6 @@ class MySelenium(NoxConSelenium):
     def __init__(self, task_info, mode):
         super().__init__(task_info=task_info, mode=mode)
 
-<<<<<<< Updated upstream
         self.capture_name = 'capture_' + self._docker_name + '.png'
         self.capture_before_name = 'capture_' + self._docker_name + '_before.png'
         self.capture_path = self._work_path + '\\Controller\\images\\temp\\' + self.capture_name
@@ -42,48 +41,6 @@ class MySelenium(NoxConSelenium):
 
         # border_path = self._work_path + '\\Controller\\images\\dianping\\border.png'
         self.border_path = self._work_path + '\\Controller\\images\\dianping\\border_128_128.png'
-=======
-    def pic_to_ocr(self, page_line=800):
-        # start = datetime.now()
-        temp_path = self._work_path + '\\Controller\\images\\temp\\'
-        capture_name = 'capture_' + self._docker_name + '.png'
-        capture_before_name = 'capture_' + self._docker_name + '_before.png'
-        capture_path = temp_path + capture_name
-        capture_before_path = temp_path + capture_before_name
-
-        capture_name_new = 'capture_' + self._docker_name + '_new.png'
-        capture_before_name_new = 'capture_' + self._docker_name + '_before_new.png'
-        capture_path_new = temp_path + capture_name_new
-        capture_before_path_new = temp_path + capture_before_name_new
-
-        capture_concat_name = 'capture_' + self._docker_name + '_concat.png'
-        capture_concat_name_new = 'capture_' + self._docker_name + '_concat_new.png'
-        capture_concat_path = temp_path + capture_concat_name
-        capture_concat_path_new = temp_path + capture_concat_name_new
-
-        # border_path = self._work_path + '\\Controller\\images\\dianping\\border.png'
-        border_path = self._work_path + '\\Controller\\images\\dianping\\border_128_128.png'
-
-        # 前后页分别截图
-        img = Image.open(capture_before_path)
-        # img = img.crop((0, 0, 480, 750))
-        img.save(capture_before_path_new)
-
-        img = Image.open(capture_path)
-        img = img.crop((0, 75, 480, page_line))
-        img.save(capture_path_new)
-        print('前后页分别截图')
-
-        # 纵向合成 800 * 480
-        img1 = cv2.imread(capture_before_path_new)
-        img2 = cv2.imread(capture_path_new)
-        img_bg = np.vstack((img1, img2))
-        cv2.imwrite(capture_concat_path, img_bg)
-        print('纵向合成')
-
-        # 照片图框 152 * 152 or 128 * 128
-        img_border = ac.imread(border_path)
->>>>>>> Stashed changes
 
     def get_photo_top_y(self, img_obj):
         # 照片图框 152 * 152 or 128 * 128
@@ -135,36 +92,23 @@ class MySelenium(NoxConSelenium):
             cv2.imwrite(self.capture_comment_path, img_concat)
 
             # 合成图截图
-<<<<<<< Updated upstream
             img = Image.open(self.capture_comment_path)
             img = img.crop((0, 180, 480, min_y - h / 2))
             img.save(self.capture_comment_cut_path)
-=======
-            img = Image.open(capture_concat_path)
-            img = img.crop((0, 0, 480, min_y - h / 2))
-            img.save(capture_concat_path_new)
->>>>>>> Stashed changes
 
             image = Image.open(self.capture_comment_cut_path)
             code = pytesseract.image_to_string(image, lang='chi_sim')
-<<<<<<< Updated upstream
             if code:
                 print('-------------------\n', code)
             else:
                 print('-------------------\n', 'not found comment')
-=======
-            code.replace('\r\n', '')
-            print('-------------------\n', code)
-        else:
-            print('-------------------\n', 'not found concat photo')
->>>>>>> Stashed changes
 
     def script(self):
         # ret, x, y = self.find_element(comment='APP打开结果OK', timeout=60)
-        # while ret:
-        self.next_page(from_y=296, to_y=100, wait_time=1)
+        # 跳过分类区
+        self.next_page(from_y=140, to_y=10, wait_time=1)
 
-        ret, x, y = self.find_element(comment='展开全文', timeout=10)
+        ret, x, y = self.find_element(comment='展开全文', timeout=5)
         if ret:
             self.click_xy(x, y, wait_time=1)
             self.get_capture()
@@ -172,8 +116,8 @@ class MySelenium(NoxConSelenium):
         # nox_adb.exe shell input swipe 240 670 240 10 2000
         # nox_adb.exe shell screencap -p /sdcard/capture.png
         # nox_adb.exe pull /sdcard/capture.png c:\Nox\
-<<<<<<< Updated upstream
-        ret, _, page_line_y = self.find_element(comment='分页线', timeout=10)
+
+        ret, _, page_line_y = self.find_element(comment='分页线', timeout=5)
         if ret:
             img_obj = Image.open(self.capture_path)
             photo_top_y = self.get_photo_top_y(img_obj)
@@ -183,15 +127,9 @@ class MySelenium(NoxConSelenium):
             img_obj = Image.open(self.capture_path)
             photo_top_y = self.get_photo_top_y(img_obj)
             if photo_top_y == -1:
-                ret, _, page_line_y = self.find_element(comment='分页线', timeout=10)
+                ret, _, page_line_y = self.find_element(comment='分页线', timeout=5)
 
         self.pic_to_ocr(one_page=ret, photo_top_y=photo_top_y, page_line_y=page_line_y)
-=======
-        ret = self.next_page(from_y=740, to_y=10, wait_time=1)
-        ret, x, y = self.find_element(comment='分页', timeout=10)
-        if ret:
-            self.pic_to_ocr(page_line=y)
->>>>>>> Stashed changes
 
         # self.back()
         # ret = self.next_page(wait_time=1)
