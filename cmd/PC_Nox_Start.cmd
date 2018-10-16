eb92f0bdc1c06941939ea90896f1b4540471e133
sougannkyou <songyq0509@163.com>
2018-10-16 09:41:09 +0800

    @company

--- cmd/PC_Nox_Start.cmd
+++ cmd/PC_Nox_Start.cmd
@@ -1,4 +1,6 @@
 cd %APPSIMULATOR_WORK_PATH%
+TASKKILL /F /T /FI "WINDOWTITLE eq WebServer - cmd\WebStart.cmd"
+TASKKILL /F /T /FI "WINDOWTITLE eq Multi Nox Monitor"
 TIMEOUT 60
 START "WebServer" cmd\WebStart.cmd
 TIMEOUT 10
